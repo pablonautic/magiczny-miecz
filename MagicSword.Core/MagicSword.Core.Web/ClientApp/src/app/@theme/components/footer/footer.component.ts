c5bfdeb44addfe4a8f3472d3f29e6780a977634c
Paweł Gorczyński <pawel.gorczynski@devfactory.com>
2018-10-26 16:25:31 +0200

    small cleanup

--- MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/@theme/components/footer/footer.component.ts
+++ MagicSword.Core/MagicSword.Core.Web/ClientApp/src/app/@theme/components/footer/footer.component.ts
@@ -1,15 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">Created by <a href="mailto:contact@plgc.eu">Paweł Gorczyński</a> 2018</span>
-    <div class="socials">
-      <a href="https://github.com/pablonautic/magiczny-miecz" target="_blank" class="ion ion-social-github"></a>
-      <a href="https://www.linkedin.com/in/pawelgorczynski/" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
-  `,
+  selector: "ngx-footer",
+  styleUrls: ["./footer.component.scss"],
+  templateUrl: "./footer.component.html",
 })
 export class FooterComponent {
 }
